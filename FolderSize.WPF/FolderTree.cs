8fea4c789e0808c76fae34b7f928cbba23e308c6
marten_range <2491891+mrange@users.noreply.github.com>
2009-10-18 22:00:39 +0000

    Replaced draw folder routine with a slightly more generic way of doing it as a preparation to make the folders clickable.

--- FolderSize.WPF/FolderTree.cs
+++ FolderSize.WPF/FolderTree.cs
@@ -76,6 +76,7 @@ namespace FolderSize.WPF
 
       Transform m_viewTransform = Transform.Identity;
       Point? m_dragPosition;
+      SizeIndex? m_buildSizeIndex;
 
       public FolderTree()
       {
@@ -116,7 +117,14 @@ namespace FolderSize.WPF
          set
          {
             m_job = value;
+            m_buildSizeIndex = null;
          }
+      }
+
+      public void Refresh()
+      {
+         m_buildSizeIndex = m_job.BuildSizeIndex ();
+         InvalidateVisual ();
       }
 
       static string ToString(long s)
@@ -151,15 +159,30 @@ namespace FolderSize.WPF
          }
       }
 
-      double DrawFolder(
-         DrawingContext drawingContext,
+      Func<CountAndSize, long> GetMeasurementPicker()
+      {
+
+         switch (DisplayMode)
+         {
+            case FolderTreeDisplayMode.Count:
+               return cs => cs.Count;
+            case FolderTreeDisplayMode.Size:
+            default:
+               return cs => cs.Size;
+         }
+      }
+
+      static double VisitFolder(
          GeneralTransform generalTransform,
          IDictionary<Folder, CountAndSize> folderCountAndSizes,
          double x,
          double y,
          double xRatio,
          double yRatio,
-         Folder folder)
+         Folder folder,
+         Func<CountAndSize, long> measurementPicker,
+         Func<long, double, Folder, Rect, bool> visit
+         )
       {
          CountAndSize countAndSize;
 
@@ -170,10 +193,10 @@ namespace FolderSize.WPF
             return 0;
          }
 
-         var measurement = PickProperty(countAndSize);
+         var measurement = measurementPicker(countAndSize);
 
          var width = xRatio - s_width;
-         var height = yRatio*measurement - s_width;
+         var height = yRatio * measurement - s_width;
 
          if (width <= 0
              || height <= 0)
@@ -196,54 +219,28 @@ namespace FolderSize.WPF
 
          var heightSquared = diffHeight.LengthSquared;
 
-         if (heightSquared <= s_minDim*s_minDim
-             || diffWidth.LengthSquared <= s_minDim*s_minDim)
+         if (heightSquared <= s_minDim * s_minDim
+             || diffWidth.LengthSquared <= s_minDim * s_minDim)
          {
             return 0;
          }
 
-         drawingContext.DrawRectangle(
-            m_folderBrush,
-            m_folderPen,
-            rect);
-
-         if (heightSquared > s_minDim*s_minDim*2.0*2.0)
-         {
-            drawingContext.PushClip(
-               new RectangleGeometry(rect));
-
-            drawingContext.DrawText(
-               new FormattedText(
-                  string.Format(
-                     "{0}\r\n{1}\r\n{2}",
-                     folder.Name,
-                     ToString(PickProperty(folder.CountAndSize)),
-                     ToString(measurement)),
-                  CultureInfo.CurrentUICulture,
-                  FlowDirection.LeftToRight,
-                  m_folderTypeFace,
-                  12.0*height/Math.Sqrt(heightSquared),
-                  Brushes.White),
-               new Point(
-                  x + 2,
-                  y + 2));
-
-            drawingContext.Pop();
-         }
+         visit(measurement, heightSquared, folder, rect);
 
          var runningY = y;
 
          foreach (var childFolder in folder.Children)
          {
-            runningY += DrawFolder(
-               drawingContext,
+            runningY += VisitFolder(
                generalTransform,
                folderCountAndSizes,
                x + xRatio,
                runningY,
                xRatio,
                yRatio,
-               childFolder);
+               childFolder,
+               measurementPicker,
+               visit);
          }
 
          return rect.Height;
@@ -332,7 +329,7 @@ namespace FolderSize.WPF
       {
          // base.OnRender(drawingContext);
 
-         if (m_job != null)
+         if (m_job != null && m_buildSizeIndex != null)
          {
             var rect = new Rect(
                 0,
@@ -349,29 +346,70 @@ namespace FolderSize.WPF
 
             drawingContext.PushTransform(m_viewTransform);
 
-            var buildSizeIndex = m_job.BuildSizeIndex();
-
-            if (buildSizeIndex.Depth > 0)
+            if (m_buildSizeIndex.Value.Depth > 0)
             {
-               var size = PickProperty(buildSizeIndex.CountsAndSizes[m_job.Root]);
+               var measurementPicker = GetMeasurementPicker ();
 
-               var xRatio = ActualWidth / buildSizeIndex.Depth;
+               var size = measurementPicker(m_buildSizeIndex.Value.CountsAndSizes[m_job.Root]);
+
+               var xRatio = ActualWidth / m_buildSizeIndex.Value.Depth;
                var yRatio = ActualHeight / size;
 
-               DrawFolder(
-                   drawingContext,
+               VisitFolder(
                    m_viewTransform,
-                   buildSizeIndex.CountsAndSizes,
+                   m_buildSizeIndex.Value.CountsAndSizes,
                    0,
                    0,
                    xRatio,
                    yRatio,
-                   m_job.Root);
+                   m_job.Root,
+                   measurementPicker,
+                   (measurement, heightSquared, folder, r) => DrawFolderImpl(drawingContext, measurement, heightSquared, folder, r));
             }
 
             drawingContext.Pop();
             drawingContext.Pop();
          }
       }
+
+      bool DrawFolderImpl (
+         DrawingContext drawingContext,
+         long measurement,
+         double heightSquared,
+         Folder folder,
+         Rect rect)
+      {
+         drawingContext.DrawRectangle(
+            m_folderBrush,
+            m_folderPen,
+            rect);
+
+         if (heightSquared > s_minDim * s_minDim * 2.0 * 2.0)
+         {
+            drawingContext.PushClip(
+               new RectangleGeometry(rect));
+
+            drawingContext.DrawText(
+               new FormattedText(
+                  string.Format(
+                     "{0}\r\n{1}\r\n{2}",
+                     folder.Name,
+                     ToString(PickProperty(folder.CountAndSize)),
+                     ToString(measurement)),
+                  CultureInfo.CurrentUICulture,
+                  FlowDirection.LeftToRight,
+                  m_folderTypeFace,
+                  12.0 * rect.Height / Math.Sqrt(heightSquared),
+                  Brushes.White),
+               new Point(
+                  rect.X + 2,
+                  rect.Y + 2));
+
+            drawingContext.Pop();
+         }
+
+         return true;
+      }
    }
 }
+   
