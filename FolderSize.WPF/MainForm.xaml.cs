f748ec30863109d31b68fd3b9f370d5aea76512f
marten_range <marten_range@localhost>
2009-10-19 13:22:02 +0000

    Fixed a null pointer exception. Fixed a race condition during start.

--- FolderSize.WPF/MainForm.xaml.cs
+++ FolderSize.WPF/MainForm.xaml.cs
@@ -24,118 +24,118 @@ using FolderSize.Common;
 
 namespace FolderSize.WPF
 {
-    partial class MainForm
-    {
-       [StructLayout(LayoutKind.Sequential)]
-       public struct MARGINS
-       {
-          public int cxLeftWidth;      // width of left border that retains its size
-          public int cxRightWidth;     // width of right border that retains its size
-          public int cyTopHeight;      // height of top border that retains its size
-          public int cyBottomHeight;   // height of bottom border that retains its size
-       };
+   partial class MainForm
+   {
+      [StructLayout(LayoutKind.Sequential)]
+      public struct MARGINS
+      {
+         public int cxLeftWidth;      // width of left border that retains its size
+         public int cxRightWidth;     // width of right border that retains its size
+         public int cyTopHeight;      // height of top border that retains its size
+         public int cyBottomHeight;   // height of bottom border that retains its size
+      };
 
 
-       [DllImport("DwmApi.dll")]
-       public static extern int DwmExtendFrameIntoClientArea(
-           IntPtr hwnd,
-           ref MARGINS pMarInset);
+      [DllImport("DwmApi.dll")]
+      public static extern int DwmExtendFrameIntoClientArea(
+          IntPtr hwnd,
+          ref MARGINS pMarInset);
 
-        public MainForm()
-        {
-            InitializeComponent();
-        }
+      public MainForm()
+      {
+         InitializeComponent();
+      }
 
-        void OnClosing(
-           object sender,
-           EventArgs e)
-        {
-           StopJob();
-        }
+      void OnClosing(
+         object sender,
+         EventArgs e)
+      {
+         StopJob();
+      }
 
-        public void OnClickGo(object sender, RoutedEventArgs value)
-        {
+      public void OnClickGo(object sender, RoutedEventArgs value)
+      {
+         StopJob();
+
+         try
+         {
+            var job = FolderTraverser.StartTraverse(PathTextBox.Text);
+            FolderTreeView.Job = job;
+         }
+         catch (Exception exc)
+         {
+            MessageBox.Show(exc.Message);
+         }
+      }
+
+      public void OnClickStop(object sender, RoutedEventArgs value)
+      {
+         StopJob();
+      }
+
+      public void OnUnloadedForm(object sender, EventArgs value)
+      {
+         try
+         {
             StopJob();
+         }
+         catch
+         {
 
-            try
+         }
+      }
+
+      void StopJob()
+      {
+         var job = FolderTreeView.Job;
+         if (job != null)
+         {
+            job.StopJob = true;
+         }
+      }
+
+      void Window_Loaded(object sender, RoutedEventArgs e)
+      {
+         try
+         {
+            // Obtain the window handle for WPF application
+            var mainWindowPtr = new WindowInteropHelper(this).Handle;
+            var mainWindowSrc = HwndSource.FromHwnd(mainWindowPtr);
+            mainWindowSrc.CompositionTarget.BackgroundColor = Color.FromArgb(
+               0,
+               0,
+               0,
+               0);
+
+            var margins =
+               new MARGINS
+               {
+                  cxLeftWidth = -1,
+                  cxRightWidth = -1,
+                  cyTopHeight = -1,
+                  cyBottomHeight = -1
+               };
+
+            var hresult =
+               DwmExtendFrameIntoClientArea(
+                  mainWindowSrc.Handle,
+                  ref margins);
+            //
+            if (hresult < 0)
             {
-                var job = FolderTraverser.StartTraverse(PathTextBox.Text);
-                FolderTreeView.Job = job;
+               throw new Exception(
+                  string.Format(
+                     "DwmExtendFrameIntoClientArea failed: {0}", hresult)
+                     );
             }
-            catch (Exception exc)
-            {
-                MessageBox.Show(exc.Message);
-            }
-        }
+         }
+         catch
+         {
+            // Fallback brush
+            Application.Current.MainWindow.Background =
+               (Brush)Application.Current.Resources["WindowGradient"];
+         }
 
-        public void OnClickStop(object sender, RoutedEventArgs value)
-        {
-            StopJob();
-        }
-
-        public void OnUnloadedForm(object sender, EventArgs value)
-        {
-            try
-            {
-                StopJob();
-            }
-            catch
-            {
-
-            }
-        }
-
-        void StopJob()
-        {
-            if (FolderTreeView.Job != null)
-            {
-                FolderTreeView.Job.StopJob = true;
-                FolderTreeView.Job = null;
-            }
-        }
-
-        void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-           try
-           {
-              // Obtain the window handle for WPF application
-              var mainWindowPtr = new WindowInteropHelper(this).Handle;
-              var mainWindowSrc = HwndSource.FromHwnd(mainWindowPtr);
-              mainWindowSrc.CompositionTarget.BackgroundColor = Color.FromArgb(
-                 0,
-                 0,
-                 0,
-                 0);
-
-              var margins =
-                 new MARGINS
-                 {
-                    cxLeftWidth = -1,
-                    cxRightWidth = -1,
-                    cyTopHeight = -1,
-                    cyBottomHeight = -1
-                 };
-
-              var hresult =
-                 DwmExtendFrameIntoClientArea(
-                    mainWindowSrc.Handle,
-                    ref margins);
-              //
-              if (hresult < 0)
-              {
-                 throw new Exception(
-                    string.Format(
-                       "DwmExtendFrameIntoClientArea failed: {0}", hresult)
-                       );
-              }
-           }
-           catch
-           {
-              // Fallback brush
-              Application.Current.MainWindow.Background =
-                 (Brush) Application.Current.Resources["WindowGradient"];
-           }
-
-        }
-    }
+      }
+   }
 }
