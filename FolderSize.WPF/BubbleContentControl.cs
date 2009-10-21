7295b9137fe985b05700820dd49114f40d431a87
marten_range <2491891+mrange@users.noreply.github.com>
2009-10-21 22:03:19 +0000

    Added new custom control used to present information to the user. The state machine for presenting info to the user should be split into parts and made more robust.

---
+++ FolderSize.WPF/BubbleContentControl.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace FolderSize.WPF
+{
+   partial class BubbleContentControl : ContentControl
+   {
+      static BubbleContentControl()
+      {
+         DefaultStyleKeyProperty.OverrideMetadata(typeof(BubbleContentControl), new FrameworkPropertyMetadata(typeof(BubbleContentControl)));
+      }
+
+      public override void OnApplyTemplate()
+      {
+         base.OnApplyTemplate();
+
+         var contentGrid = (FrameworkElement)GetTemplateChild("ContentGrid");
+         contentGrid.SizeChanged += ContentGridSizeChanged;
+      }
+
+      void ContentGridSizeChanged(object sender, SizeChangedEventArgs e)
+      {
+         var margin = ContentRectMargin;
+
+         ContentRect = new Rect(
+            margin.Left, 
+            margin.Top,
+            e.NewSize.Width - margin.Left - margin.Right,
+            e.NewSize.Height - margin.Bottom - margin.Top);
+      }
+
+      partial void OnContentRectMarginPropertyChangedPartial(Thickness oldValue, Thickness newValue)
+      {
+         var contentRect = ContentRect;
+
+         ContentRect = new Rect(
+            newValue.Left,
+            newValue.Right,
+            contentRect.Width - newValue.Left - newValue.Right + oldValue.Left + oldValue.Right,
+            contentRect.Height - newValue.Top - newValue.Bottom + oldValue.Top + oldValue.Bottom
+            );
+
+      }
+
+   }
+}
